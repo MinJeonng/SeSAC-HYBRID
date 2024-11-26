ad6196e23b0e3c97fc25c8787c47b655a7a367e3
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-11-27 01:12:11 +0900

    🔻 fix : 하위 레포지토리 삭제 및 재 업로드

---
+++ class-web/src/app/section01/01-04-webview-with-next/page.tsx
@@ -0,0 +1,14 @@
+export default function WebViewWithNextPage() {
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'purple',
+        color: 'white',
+      }}
+    >
+      이 화면은 Next web 페이지입니다. 이걸 노치 영역 제외하고 꽉 채울겁니다.
+    </div>
+  );
+}
